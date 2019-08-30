2d26a57bdc7440a2f8d6981d1d19b749c40d31c9
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-08-30 18:58:08 -0500

    tested all the reducers

--- src/reducers.test.js
+++ src/reducers.test.js
@@ -26,3 +26,58 @@ describe('searchRobots', () => {
 		});
 	});
 });
+
+describe('requestRobots', () => {
+	const initialStateRobots = {
+		robots: [],
+		isPending: false
+	};
+	it('should return the intial state', () => {
+		expect(reducers.requestRobots(undefined, {})).toEqual(initialStateRobots);
+	});
+	it('should handle REQUEST_ROBOTS_PENDING action', () => {
+		expect(
+			reducers.requestRobots(initialStateRobots, {
+				type: REQUEST_ROBOTS_PENDING
+			})
+		).toEqual({
+			robots: [],
+			isPending: true
+		});
+	});
+	it('should handle REQUEST_ROBOTS_SUCCESS action', () => {
+		expect(
+			reducers.requestRobots(initialStateRobots, {
+				type: REQUEST_ROBOTS_SUCCESS,
+				payload: [
+					{
+						id: '123',
+						name: 'test',
+						email: 'test@gmail.com'
+					}
+				]
+			})
+		).toEqual({
+			robots: [
+				{
+					id: '123',
+					name: 'test',
+					email: 'test@gmail.com'
+				}
+			],
+			isPending: false
+		});
+	});
+	it('should handle REQUEST_ROBOTS_FAILED action', () => {
+		expect(
+			reducers.requestRobots(initialStateRobots, {
+				type: REQUEST_ROBOTS_FAILED,
+				payload: 'noooo'
+			})
+		).toEqual({
+			error: 'noooo',
+			robots: [],
+			isPending: false
+		});
+	});
+});
