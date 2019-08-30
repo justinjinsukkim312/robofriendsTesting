b1b84541fa1cdb89512b62a81950e68a658a921c
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-08-30 01:51:06 -0500

    snapshot testing with state

---
+++ src/components/CounterButton.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CounterButton from './CounterButton';
+// shallow only renders what it calls, not its children
+// mount does full render and mounts the component on the dom, tests can affect each other
+// render uses cheerio and renders children
+
+it('expect to render CounterButton component', () => {
+	const mockColor = 'red';
+	expect(shallow(<CounterButton color={mockColor} />)).toMatchSnapshot();
+});
+
+it('correctly increments the counter', () => {
+	const mockColor = 'red';
+	const wrapper = shallow(<CounterButton color={mockColor} />);
+
+	wrapper.find('[id="counter"]').simulate('click');
+	expect(wrapper.state()).toEqual({ count: 2 });
+});
